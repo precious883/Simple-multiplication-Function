c4a3f68805782eec3c1da84d3303ced99c412b93
precious883 <preciouspeter25@yahoo.com>
2024-09-03 22:20:03 +0100

    multiply function

--- functions/multiply-divide.js
+++ functions/multiply-divide.js
@@ -1,5 +1,29 @@
-function math (multiply,divide){
-    return  (multiply * multiply) / (divide)
+// function math (g,h){
+//     return g/h
+// }
+// const result= math (120,3)
+// console.log ("answer",result)
+
+// function Tax (paycheck,taxPercentage){
+//     return paycheck - (paycheck * taxPercentage)
+// }
+// const Takehome= Tax (70000,0.3)
+// console.log("calculated tax" ,Takehome)
+
+// function Tax (paycheck,taxPercentage){
+//     return paycheck - (paycheck * taxPercentage)
+// }
+// const takehome = Tax (100000,0.3)
+// console.log("calculated tax" ,takehome)
+
+// function tax (salary,calculatedSalary){
+//     return salary - (salary * calculatedSalary)
+// }
+// const takehomeNG = tax (70000,0.1)
+// console.log("calculatedTax" , takehomeNG)
+
+function numbers(g,h,e){
+    return (g * h) / e
 }
-const result = math (75,4)
-console.log("math answer", result)
+const solution = numbers (4,4,4) 
+console.log("answer",solution)
